cfe73e14107243adfc4bad6be69ea0f9d526f3bf
Vitor Franco <vitoroliveirafranco@gmail.com>
2022-11-02 13:48:13 -0300

    ngif

---
+++ src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngif',
+  templateUrl: './diretiva-ngif.component.html',
+  styleUrls: ['./diretiva-ngif.component.scss'],
+})
+export class DiretivaNgifComponent implements OnInit {
+  cursos: string[] = ['Angular 2'];
+
+  mostrarCursos = false;
+
+  mostrarOuEsconderCursos() {
+    this.mostrarCursos = !this.mostrarCursos;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
